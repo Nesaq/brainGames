e836ca32fa0d04d97b40f4d8a53a7f0fc1287398
NES <kholiknes@gmail.com>
2021-12-15 18:17:41 +0300

    fix 4.3 github-action

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const userStart = () => {
+const userStart = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+export default userStart;
