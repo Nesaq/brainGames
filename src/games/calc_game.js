9f6b8db30e9902d0546857a7d31a42516cbfe48d
NES <kholiknes@gmail.com>
2021-12-28 01:00:55 +0300

    fix

--- src/games/calc_game.js
+++ src/games/calc_game.js
@@ -1,11 +1,8 @@
 import generateRandomNumber from '../fnRandomNumber.js';
 import gameStart from '../index.js';
 
-const operators = ['+', '-', '*'];
-const operatorsArrLength = operators.length;
-
-const calculation = (num1, operator, num2) => {
-  switch (operator) {
+const calculation = (num1, operators, num2) => {
+  switch (operators) {
     case '+':
       return num1 + num2;
     case '-':
@@ -18,11 +15,12 @@ const calculation = (num1, operator, num2) => {
 };
 
 const gameQuestion = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
 
 const getAnswerTheQuestion = () => {
   const num1 = generateRandomNumber(30, 1);
   const num2 = generateRandomNumber(30, 1);
-  const randomOperator = operators[generateRandomNumber(0, operatorsArrLength)];
+  const randomOperator = operators[generateRandomNumber(0, operators.length)];
   const questionForUser = `${num1} ${randomOperator} ${num2}`;
   const correctAnswer = String(calculation(num1, randomOperator, num2));
 
