aef17e26004d35bd0a8a5002f9fad23f75995c04
NES <kholiknes@gmail.com>
2021-12-28 01:25:08 +0300

    indexprogression

--- src/games/progression_game.js
+++ src/games/progression_game.js
@@ -2,22 +2,22 @@ import generateRandomNumber from '../fnRandomNumber.js';
 import gameStart from '../index.js';
 
 const getProgression = (start, stepOfProgression, progressionLength) => {
-  const progression = [];
+  const progressionArr = [];
 
   for (let i = 0; i < progressionLength; i += 1) {
-    progression.push(start + stepOfProgression * i);
+    progressionArr.push(start + stepOfProgression * i);
   }
-  return progression;
+  return progressionArr;
 };
 
 const gameQuestion = 'What number is missing in the progression?';
 
 const progressionGame = () => {
   const start = generateRandomNumber(0, 0);
-  const stepOfProgression = generateRandomNumber(5, 15);
+  const stepOfProgression = generateRandomNumber(2, 8);
   const progressionLength = generateRandomNumber(5, 10);
   const progression = getProgression(start, stepOfProgression, progressionLength);
-  const index = generateRandomNumber(2, 8);
+  const index = generateRandomNumber(2, progressionLength - 1);
   const correctAnswer = String(progression[index]);
   progression[index] = '..';
   const questionForUser = progression.join(' ');
